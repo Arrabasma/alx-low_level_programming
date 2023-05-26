668cd1b111b275393053f4eff3da9f232bc54935
Basma <arrayechbasma@gmail.com>
2023-05-25 20:07:34 -0700

    task 8

---
+++ 0x08-recursion/wildcmp.c
@@ -0,0 +1,7 @@
+#include "wildcmp.h"
+
+int wildcmp(const char *str1, const char *str2) {
+    // Your implementation of wildcmp function goes here
+    // ...
+    return 0; // Placeholder return value
+}
---
+++ 0x08-recursion/wildcmp.h
@@ -0,0 +1,6 @@
+#ifndef WILDCMP_H
+#define WILDCMP_H
+
+int wildcmp(const char *str1, const char *str2);
+
+#endif /* WILDCMP_H */
