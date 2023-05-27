2b4f7f65c2efaa73a78d395cb4afef3e94bdb6ed
Basma <arrayechbasma@gmail.com>
2023-05-27 13:34:53 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,31 @@
 #include "3-calc.h"
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
- * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
- * @s: The operator passed as argument.
+ * get_op_func - Selects the correct function to perform the operation
+ * @s: The operator passed as an argument
  *
- * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
+ * Return: A pointer to the function that corresponds to the operator,
+ *         or NULL if no matching operator is found
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL},
-	};
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-	int i = 0;
+    while (ops[i].op != NULL)
+    {
+        if (*(ops[i].op) == *s && *(s + 1) == '\0')
+            return (ops[i].f);
+        i++;
+    }
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
-
-	return (ops[i].f);
+    return (NULL);
 }
